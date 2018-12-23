e989da1c1e12c14b2f42f2fa9143224f6261e259
KingSun27 <270403579@qq.com>
2018-12-23 21:44:38 +0800

    finish the project

--- mathematical_expression_recognition/model.py
+++ mathematical_expression_recognition/model.py
@@ -10,7 +10,7 @@ def get_model():
 	model = Sequential()
 	model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64,300,1)))
 	model.add(Conv2D(32, (3, 3)))
-	model.add(Dropout(0.2))
+#	model.add(Dropout(0.4))
 	model.add(BatchNormalization())
 	model.add(Activation('relu'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -18,19 +18,19 @@ def get_model():
 	model.add(Conv2D(64, (3, 3), activation='relu'))
 	model.add(Conv2D(64, (3, 3)))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
-	model.add(Dropout(0.2))
+#	model.add(Dropout(0.4))
 	model.add(BatchNormalization())
 	model.add(Activation('relu'))
 
 	model.add(Conv2D(128, (3, 3), activation='relu'))
 	model.add(Conv2D(128, (3, 3)))
-	model.add(Dropout(0.2))
+#	model.add(Dropout(0.4))
 	model.add(BatchNormalization())
 	model.add(Activation('relu'))
 
 	model.add(Conv2D(256, (3, 3), activation='relu'))
 	model.add(Conv2D(256, (3, 3)))
-	model.add(Dropout(0.2))
+#	model.add(Dropout(0.4))
 	model.add(BatchNormalization())
 	model.add(Activation('relu'))
 
@@ -40,16 +40,16 @@ def get_model():
 	model.add(Reshape((11,100)))
 
 	model.add(CuDNNLSTM(128, return_sequences=True))
-	model.add(Dropout(0.2))
+#	model.add(Dropout(0.4))
 
 	model.add(CuDNNLSTM(128, return_sequences=True))
-	model.add(Dropout(0.2))
+#	model.add(Dropout(0.4))
 
 	model.add(CuDNNLSTM(256, return_sequences=True))
-	model.add(Dropout(0.2))
+#	model.add(Dropout(0.4))
 
 	model.add(CuDNNLSTM(256, return_sequences=True))
-	model.add(Dropout(0.2))
+#	model.add(Dropout(0.4))
 
 	model.add(TimeDistributed(Dense(17)))
 	model.add(Activation('softmax'))
